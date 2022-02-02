2d7d7cc1023a8f513297c59ae682085aeb615115
Night-Cheza <leila.kassi@outlook.com>
2022-02-02 16:29:10 -0700

    Servlets created

---
+++ src/main/java/ca/sait/login/servlets/LoginServlet.java
@@ -0,0 +1,51 @@
+package ca.sait.login.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Leila Nalivkina, Nick Hemnett
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request servlet request
+	 * @param response servlet response
+	 * @throws ServletException if a servlet-specific error occurs
+	 * @throws IOException if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if(username == null || username.isEmpty() || password == null || password.isEmpty()) {
+			request.setAttribute("message", "Username or password is missing.");
+		} else {
+
+		}
+
+		getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);		
+	}
+}
